55b69b64c9179706b7a0789726f62adf524455b6
MatiasBonesi <matiasbonesi@gmail.com>
2024-10-23 12:30:25 -0300

    Agregue para que se autoincremente el id de cursos

--- ProyectoFinal/src/main/java/com/example/ProyectoFinal/Entidad/Curso.java
+++ ProyectoFinal/src/main/java/com/example/ProyectoFinal/Entidad/Curso.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -16,6 +18,7 @@ import jakarta.persistence.Table;
 @Table(name="cursos")
 public class Curso {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@ManyToOne
 	@JoinColumn(name="tema_id")
